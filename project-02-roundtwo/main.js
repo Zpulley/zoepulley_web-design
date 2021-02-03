e7fb86bbab8efce08a602fa0df2ca06aa2ccbe11
Zpulley <77287249+Zpulley@users.noreply.github.com>
2021-02-03 17:18:36 -0500

    1

---
+++ project-02-roundtwo/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('object').jqFloat({
+		width: 100,
+		height: 100,
+		speed: 1000
+	});
+});
+  
+
--- project-02/main.js
+++ project-02/main.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
+
 	$('object').jqFloat({
 		width: 100,
 		height: 100,
 		speed: 1000
 	});
+
+  
+
+floating({
+	content: "💘",
+	number: 3,
+	duration: 11
+  });
+  floating({
+	content: "🍉",
+	number: 5,
+	duration: 8
+  });
+  floating({
+	content: "🍊",
+	number: 5,
+	duration: 15
+
+  });
+  floating({
+	content: "🤙🏾",
+	number: 1,
+	duration: 10,
+	size: 6
+  });
+
+$("divbutton").hover(function( ) {$(div).css("background-image", "url(img/hibutton-02.png)"); 
 });
-	
