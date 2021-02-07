be7efb07cc8b8997f19aecd0e853e090b80278b5
Zpulley <77287249+Zpulley@users.noreply.github.com>
2021-02-06 22:01:08 -0500

    1

--- project-02/process/process.js
+++ project-02/process/process.js
@@ -1,16 +1,32 @@
 $(document)
-    .ready(function () {
+
+// attempting to call the hover effect for "#lets-chat"
+$(".home").hover(function() {
+  $(".logo").toggle();
+  $("#lets-chat").toggle();
+  var str = "Free Web Building Tutorials!";
+var result = str.link("https://www.w3schools.com");
+});
+
+// attempting to call the hover effect for "#starburst"
+$(".process").hover(function() {
+  $(".process").toggle();
+  $("#starburst").toggle();
+  var str = "Free Web Building Tutorials!";
+var result = str.link("https://www.w3schools.com");
+});
+
+
+
+// attempting to set a slide in from right page transition
+.ready(function () {
     	$(".body").hide();
-    	// trying to have this page slide in from the right but not working. 
-
-
+    
         setTimeout(function(){ showHomePage(); }, 3000);
 
         function showHomePage(){
-            $(".body").show("slide", { direction: "left" }, 1000);
+            $(".body").show("slide", { direction: "right" }, 1000);
         }
-
-
         $(".body").click(function(e){
         	window.location.href = "http://linkToOtherPage.com/Process/";
         	
@@ -18,16 +34,8 @@ $(document)
         
     })
 
-    $("a").hover(function() {
-		$("#lets-chat").toggle();
-		$(".logo").toggle();
-		
-    });
-    
-
-    
+// attempting to make the emoji svg run
    var textPath=document.querySelector('#emoji-curve');
-   //EMOJI SVG - can't figure out why my animation will not run. i'm trying to have the text within the textpath run along the svg path
 
    function updateTextPathOffset(offset){
        textPath.setAttribute('startOffset',offset);
@@ -44,6 +52,7 @@ $(document)
    window.addEventListener('scroll',onScroll);
 
 
+// hamburger menu for mobile - this works!
    function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
