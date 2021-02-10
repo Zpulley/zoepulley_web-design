7e88ee5fedcb9dde1ea49fce89e613fdd061b2e5
Zpulley <77287249+Zpulley@users.noreply.github.com>
2021-02-10 01:06:49 -0500

    1

--- project-02/contact/contact.js
+++ project-02/contact/contact.js
@@ -1,31 +1,13 @@
 $(document).ready(function() {
     
-// attempting to set a slide in from bottom
-$(".body").hide();
+    $(".body").hide();
 
-    setTimeout(function(){ showContactPage(); }, 3000);
-
-    function showContactPage(){
-        $(".body").show("slide", { direction: "bottom" }, 1000);
+    setTimeout(function(){ showHomePage(); }, 3000);
+    
+    function showHomePage(){
+    $(".body").show("slide", { direction: "right" }, 1000);
+    
     }
-    $(".body").click(function(e){
-        window.location.href = "https://zpulley.github.io/zoepulley_web-design/project-02/process/process.html";
-        
-    })
     
 })
 
-// attempting to make the emoji svg run
-var textPath=document.querySelector('#emoji-curve');
-
-function updateTextPathOffset(offset){
-    textPath.setAttribute('startOffset',offset);
-}
-
-updateTextPathOffset(50);
-function onScroll(){
-    requestAnimationFrame(function(){
-        updateTextPathOffset(window.scrollX*1.5);
- 
-    });
-}
--- project-02/work/work.js
+++ project-02/work/work.js
@@ -8,53 +8,7 @@ $(".logo").hover(function() {
 });
 
 
-// attempting to set a slide in from right page transition
-$(".body").hide();
-    
-setTimeout(function(){ showWorkPage(); }, 3000);
 
-function showWorkPage(){
-    $(".body").show("slide", { direction: "right" }, 1000);
-};
-
-
-// attempting make the "EMOJI SVG'" slide out towards the left once you enter the page
-$(".emoji-curve").hide();
-    
-setTimeout(function(){ showWorkPage(); }, 3000);
-
-function showWorkPage(){
-    $(".emoji-curve").show("slide", { direction: "left" }, 1000);
-};
-
-
-// attempting make the ".tcontainer'" slide out towards the left once you enter the page
-$(".tcontainer").hide();
-
-setTimeout(function(){ showHomePage(); }, 3000);
-
-function showHomePage(){
-$(".tcontainer").show("slide", { direction: "right" }, 1000);
-updateTextPathOffset(50);
-}
-
-
-// attempting to make the emoji svg run
-   var textPath=document.querySelector('#emoji-curve');
-
-   function updateTextPathOffset(offset){
-       textPath.setAttribute('startOffset',offset);
-   }
-
-   updateTextPathOffset(50);
-   function onScroll(){
-       requestAnimationFrame(function(){
-           updateTextPathOffset(window.scrollX*1.5);
-    
-       });
-   }
-
-   window.addEventListener('scroll',onScroll);
 
 
 // hamburger menu for mobile - this works!
